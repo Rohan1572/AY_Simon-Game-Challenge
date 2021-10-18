6b8b6cc667f3305797349f7ce884549036dec4c8
Rohan1572 <hellorohan02@gmail.com>
2021-10-18 19:48:44 +0530

    1948

--- game.js
+++ game.js
@@ -6,4 +6,13 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 3 + 1);
   var randomChosenColor = buttonColours[randomNumber];
   gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
